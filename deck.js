6595c9580d0f08ba3d86a57cddb7e18c649ae76a
Joel K <duveled@gmail.com>
2022-02-09 15:40:11 +0100

    first commit

---
+++ deck.js
@@ -0,0 +1,125 @@
+//const fs = require("fs");
+
+//klass för korten
+class Card {
+  constructor(rank, suit) {
+    this.suit = suit;
+    this.rank = rank;
+  }
+}
+
+// klass för deck med kort
+export default class Deck {
+  constructor() {
+    //en tom array som kan fyllas med kort.
+    this.deck = [];
+  }
+  //skapa en kortlek
+  createDeck(suits, ranks) {
+    //loopa igenom bägge och lägg till i deck
+    for (let suit of suits) {
+      for (let rank of ranks) {
+        this.deck.push(new Card(rank, suit));
+      }
+    }
+    return this.deck;
+  }
+
+  //för att simpelt hämta length på array deck
+  get numberOfCards() {
+    return this.deck.length;
+  }
+
+  //blanda korten
+  shuffle() {
+    for (var i = 0; i < 52; i++) {
+      // välj ett kort
+      let tempCard = this.deck[i];
+      //en variabel för den slumpmässiga plats som kortet flyttas till
+      let randomIndex = Math.floor(Math.random() * 52);
+      //byt plats
+      this.deck[i] = this.deck[randomIndex];
+      this.deck[randomIndex] = tempCard;
+    }
+
+    /* //loopa från slut till början KOLLA FÖRKLARING PÅ DETTA,  https://www.youtube.com/watch?v=NxRwIZWjLtE
+      for (let i = this.numberOfCards - 1; i > 0; i--) {
+        const newIndex = Math.floor(Math.random() * (i + 1));
+        const oldValue = this.deck[newIndex];
+        this.deck[newIndex] = this.deck[i];
+        this.deck[i] = oldValue;
+      }*/
+    return this.deck;
+  }
+
+  //dela ut kort.
+  deal() {
+    //ny array
+    let cardOnHand = [];
+    while (cardOnHand.length < 1) {
+      //flytta kort. shift för att översta kortet
+      cardOnHand.push(this.deck.shift());
+    }
+    //skicka till nästa funktion
+    this.hand(cardOnHand);
+    //console.log(cardOnHand);
+    return cardOnHand;
+  }
+
+  //Denna är inte klar, måste göra ett back-end projekt på riktigt så att jag kan fetcha json.
+  hand(cardOnHand) {
+    for (const test of cardOnHand) {
+      console.log(test.suit, test.rank);
+    }
+
+    /* const jsonContent = JSON.stringify(cardOnHand);
+      fs.writeFile("/hand.json", jsonContent, "utf8", function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Sparad som hand.json");
+      });*/
+  }
+
+  //sortera
+  sortDeck() {
+    let sortedDeck = [];
+    //specifiera ordningen på korten, rank
+    const rankOrder = [
+      "ace",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "jack",
+      "queen",
+      "king",
+    ];
+    sortedDeck.push(
+      //higher order function. localeCompare
+      this.deck.sort(
+        (a, b) =>
+          a.suit.localeCompare(b.suit) ||
+          rankOrder.indexOf(a.rank) - rankOrder.indexOf(b.rank)
+      )
+    );
+    //denna fungerar också, men något längre
+    //(a.suit > b.suit || (a.suit == b.suit && rankOrder.indexOf(a.rank) - rankOrder.indexOf(b.rank)
+    return sortedDeck;
+  }
+  //återställ
+  resetDeck(suits, ranks) {
+    //loopa igenom bägge och lägg till i deck
+    for (let suit of suits) {
+      for (let rank of ranks) {
+        this.deck.push(new Card(rank, suit));
+      }
+    }
+    return this.deck;
+  }
+}
---
+++ main.js
@@ -0,0 +1,79 @@
+"use strict";
+import Deck from "./deck.js";
+const suits = ["diamonds", "clubs", "hearts", "spades"];
+const ranks = [
+  "ace",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "jack",
+  "queen",
+  "king",
+];
+
+//instans
+const deck = new Deck();
+//variabler
+let createBTN = document.getElementById("createDeck_btn");
+let shuffleBTN = document.getElementById("shuffleDeck_btn");
+let dealBTN = document.getElementById("dealHand_btn");
+let sortBTN = document.getElementById("sortDeck_btn");
+let resetBTN = document.getElementById("resetDeck_btn");
+let outPut = document.getElementById("message");
+
+//eventlyssnare
+//skapa kortlek
+createBTN.addEventListener("click", function () {
+  deck.createDeck(suits, ranks);
+  CreatedMessage();
+  console.log(deck);
+  disableCreate();
+});
+//blanda kortlek
+shuffleBTN.addEventListener("click", function () {
+  deck.shuffle();
+  shuffledMessage();
+  console.log(deck);
+});
+//ta ett kort
+dealBTN.addEventListener("click", function () {
+  deck.deal();
+  renderHand();
+});
+//sortera
+sortBTN.addEventListener("click", function () {
+  deck.sortDeck();
+  console.log(deck);
+  sortedMessage();
+});
+//ladda om sidan
+resetBTN.addEventListener("click", function () {
+  refreshPage();
+});
+//funktioner
+function CreatedMessage() {
+  outPut.innerHTML = "Kortlek skapad!";
+}
+function shuffledMessage() {
+  outPut.innerHTML = "Kortlek blandad!";
+}
+function sortedMessage() {
+  outPut.innerHTML = "Kortlek sorterad!";
+}
+
+//disable create knapp så att inte fler kort läggs till i kortleken
+function disableCreate() {
+  createBTN.disabled = true;
+}
+//ladda om sidan
+function refreshPage() {
+  window.location.reload();
+}
+
+function renderHand() {}
